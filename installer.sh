0140f4a77d902b382473cb286f46fb274a26fba7
CurtTeunissen <curtteunissen@msn.com>
2020-06-05 16:06:31 -0500

    Update installer.sh

--- installer.sh
+++ installer.sh
@@ -14,10 +14,10 @@ warn "via a Docker container."
 warn ""
 echo 'Please typ "not supported" to continue this installation'
 read x
-if [ "$x" != "not supported" ]
+if [ "$x" != "not supported" ];
 then
-  echo "OK, bye!"
-  exit 1
+  echo "OK, bye!";
+  exit 1;
 fi
 
 ARCH=$(uname -m)
